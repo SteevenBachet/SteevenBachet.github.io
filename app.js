8d4e5261eff98b81a17daa85096e9b87d7831742
Steeven Bachet <steevy72500@gmail.com>
2023-04-11 23:26:59 +0200

    site personnel version finale v1.2

--- app.js
+++ app.js
@@ -87,19 +87,18 @@ modalBtns.forEach((btn) => {
     const modal = btn.parentNode.parentNode.querySelector(".modal");
 
     modal.style.display = "block";
-    btnMenu.style.display = "none"
+    btnMenu.classList.add("modal-active")
 
     const closeModalBtn = modal.querySelector(".close");
     closeModalBtn.addEventListener("click", () => {
       modal.style.display = "none";
-      btnMenu.style.display = "block"
+      btnMenu.classList.remove("modal-active")
     });
 
     window.addEventListener("click", (event) => {
       if (event.target == modal) {
         modal.style.display = "none";
-        btnMenu.style.display = "block"
-        
+        btnMenu.classList.remove("modal-active")
       }
     });
   });
